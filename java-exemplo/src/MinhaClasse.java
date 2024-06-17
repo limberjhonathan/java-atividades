cc0833d459f1b32cffa4fcff1a2367a7d8d882d4
Limber jhonathan <limberjhonathan@gmail.com>
2024-06-17 19:14:40 -0300

    Initial commit

---
+++ java-exemplo/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        int nota = 5;
+
+        if (nota > 6) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
